7695de20072cf8b4e6fe963dbe86c43fc8d3daf6
Kashif Raza <kashifraza08012001@gmail.com>
2022-12-03 21:51:54 +0530

    Update db_connect.js

--- controller/db_connect.js
+++ controller/db_connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URI = "";
+const DB_URI = "mongodb+srv://datawebapp:m5rgUw6w8HGJMJtw@web.zz78grr.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
